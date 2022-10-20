82d95e7fe5a31ccad1fd2a9e7a894f04aabbcdff
0AvinashMohanDev1 <avinashmohandev@gmail.com>
2022-10-20 13:19:06 +0530

    Program 1 is checked, Program 2 need to be checked

--- prime.js
+++ prime.js
@@ -1,13 +1,13 @@
 let N=15;
-for(let i=1;i<15;i++){
+for(let i=1;i<=N;i++){
 if(N%i==0){
 count++;
 }
 }
 
-if(count>2){
-console.log("prime number")
+if(count<=2){
+console.log("prime number");
 }
 else{
-console.log("not a prime number)
+console.log("not a prime number);
 }
